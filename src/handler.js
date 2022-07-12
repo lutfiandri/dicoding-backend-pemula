5e3ff59fe71955ed69d5cc2125d3087106192432
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-07-12 14:30:22 +0700

    feat: Kriteria 5 : API dapat menghapus buku

--- src/handler.js
+++ src/handler.js
@@ -176,9 +176,34 @@ const editBookByIdHandler = (request, h) => {
     .code(200);
 };
 
+const deleteBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const idx = books.findIndex((book) => book.id === bookId);
+
+  if (idx === -1) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Buku gagal dihapus. Id tidak ditemukan',
+      })
+      .code(404);
+  }
+
+  books.splice(idx, 1);
+
+  return h
+    .response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    })
+    .code(200);
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
   editBookByIdHandler,
+  deleteBookByIdHandler,
 };
