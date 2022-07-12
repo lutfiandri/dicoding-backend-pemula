2d7a4135d1c671b38458ce48704b1db76281860e
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-07-12 13:54:16 +0700

    feat: Kriteria 1 : API dapat menyimpan buku

--- src/server.js
+++ src/server.js
@@ -1,1 +1,23 @@
-console.log('hii');
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
+    port: 5000,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+
+  // eslint-disable-next-line no-console
+  console.log(`Bookshelf API running on ${server.info.uri}`);
+};
+
+init();
